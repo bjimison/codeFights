35efdf3c7a8c7de58c6f25b648c6e6faa1b276f2
Brady <brady.t.jimison@gmail.com>
2017-11-21 21:16:19 -0800

    arrayChange.js & commonCharacter.js

--- arrayChange.js
+++ arrayChange.js
@@ -1,3 +1,11 @@
+You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+Example
+
+For inputArray = [1, 1, 1], the output should be
+arrayChange(inputArray) = 3.
+
+
 function arrayChange(inputArray) {
 // if inputArray[i] < inputArray[i + 1]
 // inputArray[i] = inputArray[i] + 1 
--- commonCharacter.js
+++ commonCharacter.js
@@ -1,3 +1,13 @@
+Given two strings, find the number of common characters between them.
+
+Example
+
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+commonCharacterCount(s1, s2) = 3.
+
+Strings have 3 common characters - 2 "a"s and 1 "c".
+
+
 function commonCharacterCount(s1, s2) {
 s1 = s1.split("");
 s2 = s2.split("");
@@ -33,3 +43,6 @@ var common2 = {};
     }
            return total; 
 }
+
+(Test cases)s1: "aabcc"
+            s2: "adcaa"
